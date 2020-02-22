4eb83625ab36015c7935a4f7304810e03a884af7
Joni Arstio <55687959+joniarstio@users.noreply.github.com>
2020-02-22 12:37:13 +0200

    Added clock files

---
+++ script.js
@@ -0,0 +1,27 @@
+const hourHand = document.querySelector("#hour");
+const minuteHand = document.querySelector("#minute");
+const secondHand = document.querySelector("#second");
+
+var date = new Date();
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+function runTheClock() {
+
+    hrPosition = hrPosition + (3/360);
+    minPosition = minPosition + (6/60);
+    secPosition = secPosition + 6;
+
+    hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+    minuteHand.style.transform = "rotate(" + minPosition + "deg)";
+    secondHand.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
